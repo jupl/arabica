d02d4e81139e81da2abdbf9bd47d67310cdaf083
jupl <juan.placencia.512@gmail.com>
2016-12-15 20:29:59 -0600

    [ci skip] Formatting

--- .webpack/index.js
+++ .webpack/index.js
@@ -16,11 +16,7 @@ const addHot = require('./hot')
  * @param {IndexOptions} options Options
  * @return {Object} Webpack configuration
  */
-module.exports = ({
-  source,
-  destination,
-  assets,
-}) => {
+module.exports = ({source, destination, assets}) => {
   // Create base configuration and export
   const config = createConfig({source, destination})
 
